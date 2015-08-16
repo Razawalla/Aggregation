2397206832f9f7d512c40e183865a13d408eda49
Bala <bkrishna.9572@gmail.com>
2015-08-16 22:45:56 +0530

    Completed this B*tch

--- src/SqlAccess.java
+++ src/SqlAccess.java
@@ -26,7 +26,7 @@ public class SqlAccess {
 	public ResultSet readDatabase()throws Exception{
 		System.out.println("came to readDatabase");
 			statement=connect.createStatement();
-			 result=statement.executeQuery("select * from marks.student group by roll");
+			 result=statement.executeQuery("select * from student group by roll");
 			 
 			 
 			return result;
@@ -96,67 +96,18 @@ public class SqlAccess {
 	}
 	
 	public ResultSet getAnything(String query) throws SQLException{
-		String args[]=query.split(" ");
-		for(int i=0;i<args.length;i++)
-		System.out.println(args[i]);
-		String selectArgs[]=args[1].split(",");
-		for(int i=0;i<selectArgs.length;i++)
-			System.out.println(selectArgs[i]);
-		String whereArgs[]=query.split("=");
 		
-		System.out.println(whereArgs[1]);
-		String condition[]=whereArgs[0].split(" ");
-		System.out.println(condition[condition.length-1]);
-		if(condition[condition.length-1].equals("name")){
-			String roll=new String();
+		if(query.contains("marks")){
+			System.out.println("came to getAnything");
+			query=query.replace("marks", "hashmap.marks");
+			query=query.replace("where", "and");
+			query=query.replace("student", "student,hashmap,roll_to_hash where student.roll=roll_to_hash.hash_roll and hashmap.roll_hash=roll_to_hash.hash and student.subject=hashmap.hash_subject");
 			statement=connect.createStatement();
-			result=statement.executeQuery("select roll from student where name="+"'"+whereArgs[1]+"'");
-			while(result.next())
-				roll=result.getString("roll");
-			System.out.println(roll);
-			String toRetrieve="select "+"t0."+selectArgs[0];
-			int k=1;
-			for(int i=0;i<selectArgs.length;i++){
-				if(selectArgs[i].contains("(")){
-					statement=connect.createStatement();
-					result=statement.executeQuery("select "+selectArgs[i]+" from marks.hashmap where roll_hash=(select hash from marks.roll_to_hash where roll="+roll+")");
-					while(result.next())
-						System.out.println(result.getString(1));
-				}
-				else{
-					System.out.println("came to else");
-					statement=connect.createStatement();
-					result=statement.executeQuery("select "+selectArgs[i]+" from student where name=");
-					while(result.next()){
-						System.out.println(result.getString(1));
-					}
-				}
-				
-			}
-			
-			
+			result=statement.executeQuery(query);
+			return result;
 		}
-		else if(condition[condition.length-1].equals("roll")){
-			for(int i=0;i<selectArgs.length;i++){
-				if(selectArgs[i].contains("(")){
-					statement=connect.createStatement();
-					result=statement.executeQuery("select "+selectArgs[i]+" from marks.hashmap where roll_hash=(select hash from marks.roll_to_hash where roll="+whereArgs[1]+")");
-					while(result.next())
-						System.out.println(result.getString(1));
-				}
-				
-				}
-			
-		}
-		
-		
-		
-		
-		 
 		return null;
-	
-		
-	}
+		}
 
 	public void close() {
 		// TODO Auto-generated method stub
